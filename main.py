a1437d405c5bef3a5a08a7d2110cf4fdd0be2fc9
Luís Mota <lhrmota@gmail.com>
2020-12-10 04:47:35 +0000

    Added rotation and put some new yellow and white balls.

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from time import sleep
 
 sense = SenseHat()
 
-X = [0, 100, 0] # Green
+G = [0, 100, 0] # Green
 O = [0, 0, 0] # Black
 W = [255, 255, 255]  # White
 B = [139, 69, 19] # Brown
 U = [0, 0, 128] # Blue
 R = [255, 0, 0] # Red
+Y = [255,255,0] # Yellow
 
 def clear_one_column_from_left(xmas_tree):
   for i in range(8):
@@ -23,17 +24,19 @@ def move_image_left(xmas_tree,i):
       return_image.append(O)
     for j in range(i):
       return_image.append(xmas_tree[line*8+j])
-  
+
   return return_image
-  
+
+# Adjust to display rotation
+sense.set_rotation(180)
 while True:
 	xmas_tree= [
-	O, O, O, X, X, O, O, O,
-	O, O, X, X, X, X, O, O,
-	O, X, X, X, X, X, X, O,
-	O, O, X, X, X, X, O, O,
-	O, X, X, X, X, X, X, O,
-	X, X, X, X, X, X, X, X,
+	O, O, O, G, G, O, O, O,
+	O, O, G, G, Y, G, O, O,
+	O, G, Y, G, G, G, G, O,
+	O, O, G, G, G, G, O, O,
+	O, G, G, G, Y, G, G, O,
+	G, G, G, G, G, G, G, G,
 	O, O, O, B, B, O, O, O,
 	O, O, O, B, B, O, O, O
 	]
@@ -52,10 +55,10 @@ while True:
 	  
 	xmas_tree = [
 	O, O, O, U, U, O, O, O,
-	O, O, U, U, U, U, O, O,
+	O, O, Y, U, U, U, O, O,
 	O, U, U, U, U, U, U, O,
-	O, O, U, U, U, U, O, O,
-	O, U, U, U, U, U, U, O,
+	O, O, U, U, Y, U, O, O,
+	O, U, U, W, U, U, U, O,
 	U, U, U, U, U, U, U, U,
 	O, O, O, B, B, O, O, O,
 	O, O, O, B, B, O, O, O
@@ -75,11 +78,11 @@ while True:
 	  
 	xmas_tree = [
 	O, O, O, W, W, O, O, O,
+	O, O, W, W, Y, W, O, O,
+	O, W, B, W, W, W, W, O,
 	O, O, W, W, W, W, O, O,
-	O, W, W, W, W, W, W, O,
-	O, O, W, W, W, W, O, O,
-	O, W, W, W, W, W, W, O,
-	W, W, W, W, W, W, W, W,
+	O, W, W, Y, W, W, W, O,
+	W, W, W, W, W, Y, W, W,
 	O, O, O, B, B, O, O, O,
 	O, O, O, B, B, O, O, O
 	]
@@ -98,11 +101,11 @@ while True:
 	  
 	xmas_tree = [
 	O, O, O, R, R, O, O, O,
-	O, O, R, R, R, R, O, O,
+	O, O, R, Y, R, R, O, O,
 	O, R, R, R, R, R, R, O,
-	O, O, R, R, R, R, O, O,
-	O, R, R, R, R, R, R, O,
-	R, R, R, R, R, R, R, R,
+	O, O, R, R, Y, R, O, O,
+	O, R, B, R, R, R, R, O,
+	R, R, R, R, Y, R, R, R,
 	O, O, O, B, B, O, O, O,
 	O, O, O, B, B, O, O, O
 	]
