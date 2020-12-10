9ed3f27af6344b21474094d449250170a7bc4eed
luismota <lhrmota@gmail.com>
2020-12-10 12:48:23 +0000

    Make lights blink also when sliding

--- main.py
+++ main.py
@@ -27,10 +27,40 @@ def move_image_left(xmas_tree,i):
 
   return return_image
 
+
+# alternate between two images to make lanterns
+# seem to blink
+def alternate_between_two_images(image1,image2,interval,numTimes):
+  for i in range(1,numTimes):
+    sense.set_pixels(image1)
+    sleep(interval)
+    sense.set_pixels(image2)
+    sleep(interval)
+
+def slide_out_while_alternating_two_images(image1,image2,interval):
+  for i in range(8):
+    clear_one_column_from_left(image1)
+    clear_one_column_from_left(image2)
+    if i%2==0:
+      sense.set_pixels(image1)
+    else:
+      sense.set_pixels(image2)
+    sleep(interval)
+
+def slide_in_while_alternating_two_images(image1,image2,interval):
+  for i in range(1,9):
+    partial_tree1=move_image_left(image1,i)
+    partial_tree2=move_image_left(image2,i)
+    if i%2==0:
+      sense.set_pixels(partial_tree1)
+    else:
+      sense.set_pixels(partial_tree2)
+    sleep(interval)
+    
 # Adjust to display rotation
 sense.set_rotation(180)
 while True:
-	xmas_tree= [
+	xmas_tree_A= [
 	O, O, O, G, G, O, O, O,
 	O, O, G, G, Y, G, O, O,
 	O, G, Y, G, G, G, G, O,
@@ -41,19 +71,24 @@ while True:
 	O, O, O, B, B, O, O, O
 	]
 
-	for i in range(1,9):
-	  partial_tree=move_image_left(xmas_tree,i)
-	  sense.set_pixels(partial_tree)
-	  sleep(.2)
+        xmas_tree_B= [
+	O, O, O, G, G, O, O, O,
+	O, O, G, G, B, G, O, O,
+	O, G, G, G, G, G, G, O,
+	O, O, G, Y, G, G, O, O,
+	O, G, G, G, G, G, G, O,
+	G, G, G, G, G, U, G, G,
+	O, O, O, B, B, O, O, O,
+	O, O, O, B, B, O, O, O
+	]
 
-	sleep(2)
+	slide_in_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
 
-	for i in range(8):
-	  clear_one_column_from_left(xmas_tree)
-	  sense.set_pixels(xmas_tree)
-	  sleep(.2)
+	alternate_between_two_images(xmas_tree_A,xmas_tree_B,.1,5)
+
+        slide_out_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
  
-	xmas_tree = [
+	xmas_tree_A = [
 	O, O, O, U, U, O, O, O,
 	O, O, Y, U, U, U, O, O,
 	O, U, U, U, U, U, U, O,
@@ -64,19 +99,25 @@ while True:
 	O, O, O, B, B, O, O, O
 	]
 
-	for i in range(1,9):
-	  partial_tree=move_image_left(xmas_tree,i)
-	  sense.set_pixels(partial_tree)
-	  sleep(.2)
+        xmas_tree_B = [
+	O, O, O, U, U, O, O, O,
+	O, O, U, W, U, U, O, O,
+	O, U, U, U, U, W, U, O,
+	O, O, U, U, U, U, O, O,
+	O, U, Y, U, U, U, U, O,
+	U, U, U, U, U, U, U, U,
+	O, O, O, B, B, O, O, O,
+	O, O, O, B, B, O, O, O
+	]
 
-	sleep(2)
+        
+	slide_in_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
 
-	for i in range(8):
-	  clear_one_column_from_left(xmas_tree)
-	  sense.set_pixels(xmas_tree)
-	  sleep(.2)
+	alternate_between_two_images(xmas_tree_A,xmas_tree_B,.1,5)
 
-	xmas_tree = [
+        slide_out_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
+
+	xmas_tree_A = [
 	O, O, O, W, W, O, O, O,
 	O, O, W, W, Y, W, O, O,
 	O, W, U, W, W, W, W, O,
@@ -87,19 +128,25 @@ while True:
 	O, O, O, B, B, O, O, O
 	]
 
-	for i in range(1,9):
-	  partial_tree=move_image_left(xmas_tree,i)
-	  sense.set_pixels(partial_tree)
-	  sleep(.2)
+        xmas_tree_B = [
+	O, O, O, W, W, O, O, O,
+	O, O, W, W, W, W, O, O,
+	O, W, W, Y, W, W, W, O,
+	O, O, U, W, W, W, O, O,
+	O, W, W, W, W, Y, W, O,
+	W, W, W, W, W, U, W, W,
+	O, O, O, B, B, O, O, O,
+	O, O, O, B, B, O, O, O
+	]
 
-	sleep(2)
+        
+	slide_in_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
 
-	for i in range(8):
-	  clear_one_column_from_left(xmas_tree)
-	  sense.set_pixels(xmas_tree)
-	  sleep(.2)
+	alternate_between_two_images(xmas_tree_A,xmas_tree_B,.1,5)
 
-	xmas_tree = [
+        slide_out_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
+
+	xmas_tree_A = [
 	O, O, O, R, R, O, O, O,
 	O, O, R, U, R, R, O, O,
 	O, R, R, R, R, R, R, O,
@@ -110,14 +157,20 @@ while True:
 	O, O, O, B, B, O, O, O
 	]
 
-	for i in range(1,9):
-	  partial_tree=move_image_left(xmas_tree,i)
-	  sense.set_pixels(partial_tree)
-	  sleep(.2)
+        xmas_tree_B = [
+	O, O, O, R, R, O, O, O,
+	O, O, R, R, Y, R, O, O,
+	O, R, W, R, R, R, R, O,
+	O, O, R, R, R, R, O, O,
+	O, R, R, R, R, R, R, O,
+	R, R, R, U, R, R, R, R,
+	O, O, O, B, B, O, O, O,
+	O, O, O, B, B, O, O, O
+	]
 
-	sleep(2)
+        
+	slide_in_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
 
-	for i in range(8):
-	  clear_one_column_from_left(xmas_tree)
-	  sense.set_pixels(xmas_tree)
-	  sleep(.2)
+	alternate_between_two_images(xmas_tree_A,xmas_tree_B,.1,5)
+
+        slide_out_while_alternating_two_images(xmas_tree_A,xmas_tree_B,.1)
